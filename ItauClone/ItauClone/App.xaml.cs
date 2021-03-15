066adbe0e55488a550fdf5fd10d76ec68e40617a
Ano Emangi <ano.emangi@minutoseguros.com.br>
2021-03-15 20:19:56 -0300

    Inciando o projeto clone

---
+++ ItauClone/ItauClone/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using ItauClone.Views;
+using Xamarin.Forms;
+
+namespace ItauClone
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginView();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ItauClone/ItauClone/Views/LoginView.cs
@@ -0,0 +1,18 @@
+﻿
+using Xamarin.Forms;
+
+namespace ItauClone.Views
+{
+    public class LoginView : ContentPage
+    {
+        public LoginView()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Welcome to Xamarin.Forms!" }
+                }
+            };
+        }
+    }
+}
